07e5d05930179ccdf2d2c338c9466548d5f972e5
Andrea Grandi <MotAtlas@gmail.com>
2022-05-20 18:57:33 +0200

    Migliorata la somma di segnali

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -9,17 +9,21 @@ DelayFXAudioProcessor::DelayFXAudioProcessor()
     parameters.addParameterListener(NAME_DT, this);
     parameters.addParameterListener(NAME_FB, this);
     parameters.addParameterListener(NAME_FREQ, this);
+    parameters.addParameterListener(NAME_FREQ_LFO2, this);
+    parameters.addParameterListener(NAME_LFO2_ON, this);
     parameters.addParameterListener(NAME_MOD, this);
     parameters.addParameterListener(NAME_WF, this);
     parameters.addParameterListener(NAME_PH_DELTA, this);
-    parameters.addParameterListener(NAME_FREQ_LFO2, this);
-    parameters.addParameterListener(NAME_LFO2_ON, this);
 
     delay.setFeedback(DEFAULT_FB);
     //delay.setTime(DEFAULT_DT);
     drywetter.setDryWetRatio(DEFAULT_DW);
+
+    //set LFO 1
     LFO.setFrequency(DEFAULT_FREQ);
     LFO.setWaveform(DEFAULT_WF);
+
+    //set LFO 2
     LFO2.setFrequency(DEFAULT_FREQ_LFO2);
     LFO2.setWaveform(DEFAULT_WF);
     timeAdapter.setModAmount(DEFAULT_MOD);
@@ -35,10 +39,20 @@ void DelayFXAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBloc
 {
     drywetter.prepareToPlay(sampleRate, samplesPerBlock);
     delay.prepareToPlay(sampleRate, samplesPerBlock);
+
+    // Init LFO 1
     LFO.prepareToPlay(sampleRate);
+
+    // Init LFO 2
     LFO2.prepareToPlay(sampleRate);
+
+    // Init modulation signal 1
     modulationSignal.setSize(2, samplesPerBlock);
+
+    // Init modulation signal 2
     modulationSignal2.setSize(2, samplesPerBlock);
+
+
     timeAdapter.prepareToPlay(sampleRate);
 }
 
@@ -46,7 +60,11 @@ void DelayFXAudioProcessor::releaseResources()
 {
     drywetter.releaseResources();
     delay.releaseResurces();
+
+    // Release resources of modulation signal 1
     modulationSignal.setSize(0, 0);
+
+    // Release resources of modulation signal 2
     modulationSignal2.setSize(0, 0);
 }
 
@@ -55,31 +73,40 @@ void DelayFXAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce
     juce::ScopedNoDenormals noDenormals;
     const auto numSamples = buffer.getNumSamples();
 
-    // Genero una modulante
+    // Scrivo su buffer modulationSignal un blocco dell'LFO 1
     LFO.getNextAudioBlock(modulationSignal, numSamples);
+    //modulationSignal2.makeCopyOf(modulationSignal);
+
+    // Scrivo su buffer modulationSignal2 un blocco dell'LFO 2
     LFO2.getNextAudioBlock(modulationSignal2, numSamples);
     
-    if (lfo2On) {
-        auto bufferData = modulationSignal.getArrayOfWritePointers();
-        auto mod = modulationSignal2.getArrayOfReadPointers();
+    if (lfo2On)  
+    {
+        auto modSig1Pointers = modulationSignal.getArrayOfWritePointers();
+        auto modSig2Pointers = modulationSignal2.getArrayOfReadPointers();
+        
+        for (int ch = 0; ch < modulationSignal.getNumChannels(); ++ch)
+            FloatVectorOperations::add(modSig1Pointers[ch], modSig2Pointers[ch], numSamples);
 
+        /*
         for (int smp = 0; smp < numSamples; ++smp)
         {
             for (int ch = 0; ch < modulationSignal.getNumChannels(); ++ch)
             {
-                auto modul = mod[ch][smp];
-                auto buf = bufferData[ch][smp];
+                auto modul = modSig1Pointers[ch][smp];
+                auto buf = modSig2Pointers[ch][smp];
 
-                bufferData[ch][smp] = buf + modul;
+                modSig1Pointers[ch][smp] = buf + modul;
             }
             
         }
+        */
 
         //modulationSignal.addFrom(0, 0, modulationSignal2, 0, numSamples, 1);
         //modulationSignal.addFrom(1, 0, modulationSignal2, 1, numSamples, 1);
     }
-    
 
+    
     // Scalare modulante
     timeAdapter.processBlock(modulationSignal, numSamples);
 
@@ -94,7 +121,8 @@ void DelayFXAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce
     drywetter.merge(buffer);
 
     // ~~~ Listen to the wavez ~~~
-    //LFO.getNextAudioBlock(buffer, buffer.getNumSamples());
+    //LFO2.getNextAudioBlock(buffer, numSamples);
+    //buffer.makeCopyOf(modulationSignal);
 }
 
 
